af58088f4d2d111d5c2dab89a8b7fdbe3099c139
SeeYoung Jin <jsee53@naver.com>
2023-01-01 00:30:19 +0900

    first commit

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,9 +1,12 @@
+var clock = 0;
+
 function lottery() {
   var number;
-  for (var i = 0; i < 7; i++) {
-    number = Math.floor(Math.random() * 44) + 1;
-    setTimeout(function () {}, 1000);
+  number = Math.floor(Math.random() * 44) + 1;
+  if (clock <= 6) {
     console.log(number);
+    clock++;
+    setTimeout(lottery(), 1000);
   }
   const numberText = document.getElementById("number");
   numberText.innerHTML = "<div>text</div>";
