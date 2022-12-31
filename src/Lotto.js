e51bada6182f3dfb7f6f1adf9e760be5cd600946
SeeYoung Jin <jsee53@naver.com>
2022-12-31 18:16:13 +0900

    second commit

--- src/Lotto.js
+++ src/Lotto.js
@@ -2,6 +2,7 @@ function lottery() {
   var number;
   for (var i = 0; i < 6; i++) {
     number = Math.floor(Math.random() * 44) + 1;
-    console.log(number);
   }
+  const numberText = document.getElementById("number");
+  numberText.innerHTML = "<div>text</div>";
 }
