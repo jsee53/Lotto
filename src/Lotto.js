4cf4404cf3ec80554a03762ca91766fc639da20b
SeeYoung Jin <jsee53@naver.com>
2023-01-02 22:53:04 +0900

    process

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,13 +1,36 @@
-var clock = 0;
+var lottoNumber = [];
 
 function lottery() {
   var number;
-  number = Math.floor(Math.random() * 44) + 1;
-  if (clock <= 6) {
+  for (var i = 0; i < 6; i++) {
+    number = Math.floor(Math.random() * 45) + 1;
+    while (!duplicationCheck(number)) {
+      number = Math.floor(Math.random() * 45) + 1;
+    }
+    sleep();
     console.log(number);
-    clock++;
-    setTimeout(lottery(), 1000);
   }
   const numberText = document.getElementById("number");
   numberText.innerHTML = "<div>text</div>";
+}
+
+//1초 딜레이 하는 함수
+function sleep() {
+  let start = Date.now();
+  let now = start;
+  while (now < start + 1000) {
+    now = Date.now();
+  }
+}
+
+//랜덤 생성한 번호의 중복을 확인하는 함수
+function duplicationCheck(num) {
+  console.log(lottoNumber.length);
+  for (var i = 0; i < lottoNumber.length; i++) {
+    if (lottoNumber[i] == num) {
+      return false;
+    }
+    console.log("text: " + lottoNumber[i]);
+  }
+  return true;
 }
